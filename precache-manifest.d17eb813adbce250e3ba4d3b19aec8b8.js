c1b503ab100fba5ca62cbd6e0623d0b933512e21
RigvedBhavsar <rigvedbhavsar@gmail.com>
2020-11-25 18:38:51 +0530

    Updates

---
+++ precache-manifest.d17eb813adbce250e3ba4d3b19aec8b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbbb3bd750e04809931e24fa47c10a85",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83f138ae438172a89425",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "753bc64de7055dfd5b3d",
+    "url": "/static/js/2.b11a5bee.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.b11a5bee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83f138ae438172a89425",
+    "url": "/static/js/main.134e81fd.chunk.js"
+  },
+  {
+    "revision": "229b32b4154aa7d2a78b",
+    "url": "/static/js/runtime-main.77db6e1b.js"
+  }
+]);
